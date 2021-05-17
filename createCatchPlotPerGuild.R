51623619547149c007c77f4f4f64ede7a9ef9151
tdoherty <thomas.doherty@strath.ac.uk>
2021-05-17 16:29:23 +0100

    Fixed plot to allow dodging and stacking simultaneously

--- createCatchPlotPerGuild.R
+++ createCatchPlotPerGuild.R
@@ -22,45 +22,58 @@ createCatchPlotPerGuild	<- function( model, results, dsa ) {
 
   offshore_data2plot_df<- data.frame(offshore_data2plot)
   inshore_data2plot_df <- data.frame(inshore_data2plot)
-  row.names(offshore_data2plot_df) <- c("offshore landings","offshore discards")
-  row.names(inshore_data2plot_df) <- c("inshore landings","inshore discards")
+  row.names(offshore_data2plot_df) <- c("landings","discards")
+  row.names(inshore_data2plot_df) <- c("landings","discards")
   offshore_data2plot_df$type <- rownames(offshore_data2plot_df)
+  offshore_data2plot_df$zone <- c("offshore")
   inshore_data2plot_df$type <- rownames(inshore_data2plot_df)
+  inshore_data2plot_df$zone <- c("inshore")
+  
 
   offshore_long <- gather(offshore_data2plot_df, gear, catch, Pelagic_Trawl.Seine:Whaler, factor_key=TRUE)
   inshore_long <- gather(inshore_data2plot_df, gear, catch, Pelagic_Trawl.Seine:Whaler, factor_key=TRUE)
   
   combined <- rbind(offshore_long,inshore_long)
+  combined$x <- factor(paste(combined$gear, combined$zone))
+  # View(combined)
+
+   barplot <-  ggplot(combined) +
+    geom_col(aes(x = as.numeric(x), y = catch, fill = paste(zone, type))) +
+    scale_x_continuous(breaks = seq(from = 1.5, to = length(unique(combined$x)), by = 2),
+                       labels = as.character(unique(combined$gear))) +
+       theme(axis.text.x = element_text(angle = 45, hjust=1)) +
+       ggtitle(mt) +
+       theme(plot.title = element_text(hjust = 0.5)) +
+       theme(plot.title = element_text(size=16)) +
+       theme(axis.title=element_text(size=14)) +
+       xlab("Gear") + ylab("Catch")
   
-  barplot <- ggplot(data=combined, aes(x=gear,y=catch, fill=type)) +
-    geom_bar( stat="identity", position ="dodge", alpha=.8) +
-    theme(axis.text.x = element_text(angle = 45, hjust=1)) +
-    ggtitle(mt) +
-    theme(plot.title = element_text(hjust = 0.5)) +
-    theme(plot.title = element_text(size=16)) +
-    theme(axis.title=element_text(size=14)) +
-    xlab("Gear") + ylab("Catch")
   return(barplot)
   }
   if(dsa == 12){
   mt <- "All guilds combined"
   offshore_data2plot <- rbind(colSums(offshore_discmat), colSums(offshore_landmat))
   inshore_data2plot <- rbind(colSums(inshore_discmat), colSums(inshore_landmat))
-
   offshore_data2plot_df<- data.frame(offshore_data2plot)
   inshore_data2plot_df <- data.frame(inshore_data2plot)
-  row.names(offshore_data2plot_df) <- c("offshore landings","offshore discards")
-  row.names(inshore_data2plot_df) <- c("inshore landings","inshore discards")
+  
+  row.names(offshore_data2plot_df) <- c("landings","discards")
+  row.names(inshore_data2plot_df) <- c("landings","discards")
   offshore_data2plot_df$type <- rownames(offshore_data2plot_df)
+  offshore_data2plot_df$zone <- c("offshore")
   inshore_data2plot_df$type <- rownames(inshore_data2plot_df)
+  inshore_data2plot_df$zone <- c("inshore")
   
   offshore_long <- gather(offshore_data2plot_df, gear, catch, Pelagic_Trawl.Seine:Whaler, factor_key=TRUE)
   inshore_long <- gather(inshore_data2plot_df, gear, catch, Pelagic_Trawl.Seine:Whaler, factor_key=TRUE)
   
   combined <- rbind(offshore_long,inshore_long)
+  combined$x <- factor(paste(combined$gear, combined$zone))
   
-  barplot <- ggplot(data=combined, aes(x=gear,y=catch, fill=type)) +
-    geom_bar( stat="identity", position ="dodge", alpha=.8) +
+  barplot <-  ggplot(combined) +
+    geom_col(aes(x = as.numeric(x), y = catch, fill = paste(zone, type))) +
+    scale_x_continuous(breaks = seq(from = 1.5, to = length(unique(combined$x)), by = 2),
+                       labels = as.character(unique(combined$gear))) +
     theme(axis.text.x = element_text(angle = 45, hjust=1)) +
     ggtitle(mt) +
     theme(plot.title = element_text(hjust = 0.5)) +
