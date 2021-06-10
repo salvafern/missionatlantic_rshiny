87c57fc3ac0bb9ab8dd5bbf58fc985f799a70d03
tdoherty <thomas.doherty@strath.ac.uk>
2021-06-10 16:55:48 +0100

    Tidies up script to get ggplot related plots writing to file

--- createPlotsForDownload.R
+++ createPlotsForDownload.R
@@ -22,6 +22,7 @@ source("createEDriversPlots.R")
 
 
 #model <- e2e_read(selectedlocation, selectedVariant,models.path="Models",model.ident = "baseline")
+
 model <- e2e_read("North_Sea", "1970-1999",models.path="Models",model.ident = "baseline")
 results_baseline <- e2e_run(model, nyears = 5, csv.output = TRUE)
 
@@ -129,7 +130,7 @@ ecoPlot_nut_phyt_Nitrate <- createEcoplots(
       selection = "NUT_PHYT",
       subSelection = "Nitrate")
 dev.off()
-    
+
 png(filename = paste0(selectedLocation, "_" ,selectedVariant, "_", "EcologicalOutput","_","ecoPlot_nut_phyt_Phytoplankton.png"))
 ecoPlot_nut_phyt_Phytoplankton <- createEcoplots(
       model,
@@ -378,221 +379,195 @@ ecoPlot_macrophyte_inshoreDeb <-    createEcoplots(
     )
 dev.off()
 
-png(filename = paste0(selectedLocation, "_" ,selectedVariant, "_", "CatchPerGuildOutput","_","planktivorousFish.png"))
 planktivorousFish <-    createCatchPlotPerGuild(
-      model,
-      results = results_baseline,
-      dsa = 1
-    )
-dev.off()
+  model,
+  results = results_baseline,
+  dsa = 1
+)
+ggsave(filename = paste0(selectedLocation, "_" ,selectedVariant, "_", "CatchPerGuildOutput","_","planktivorousFish.png"),planktivorousFish,  width = 12, height = 4, dpi = 600, units = "in", device= "png")
 
-png(filename = paste0(selectedLocation, "_" ,selectedVariant, "_", "CatchPerGuildOutput","_","Quota_limited_Demersal_fish.png"))
+
 Quota_limited_Demersal_fish <-    createCatchPlotPerGuild(
       model,
       results = results_baseline,
       dsa = 2
     )
-dev.off()
+ggsave(filename = paste0(selectedLocation, "_" ,selectedVariant, "_", "CatchPerGuildOutput","_","Quota_limited_Demersal_fish.png"),Quota_limited_Demersal_fish,  width = 12, height = 4, dpi = 600, units = "in", device= "png")
 
-png(filename = paste0(selectedLocation, "_" ,selectedVariant, "_", "CatchPerGuildOutput","_","Non_quota_demersal_fish.png"))
 Non_quota_demersal_fish <-     createCatchPlotPerGuild(
       model,
       results = results_baseline,
       dsa = 3
     )
-dev.off()
+ggsave(filename = paste0(selectedLocation, "_" ,selectedVariant, "_", "CatchPerGuildOutput","_","Non_quota_demersal_fish.png"),Non_quota_demersal_fish,  width = 12, height = 4, dpi = 600, units = "in", device= "png")
 
-png(filename = paste0(selectedLocation, "_" ,selectedVariant, "_", "CatchPerGuildOutput","_","Migratory_fish.png"))
 Migratory_fish <-     createCatchPlotPerGuild(
       model,
       results = results_baseline,
       dsa = 4
     )
-dev.off()
+ggsave(filename = paste0(selectedLocation, "_" ,selectedVariant, "_", "CatchPerGuildOutput","_","Migratory_fish.png"),Migratory_fish,  width = 12, height = 4, dpi = 600, units = "in", device= "png")
 
-png(filename = paste0(selectedLocation, "_" ,selectedVariant, "_", "CatchPerGuildOutput","_","Susp_deposit_feeding_benthos.png"))
 Susp_deposit_feeding_benthos <-     createCatchPlotPerGuild(
       model,
       results = results_baseline,
       dsa = 5
     )
-dev.off()
+ggsave(filename = paste0(selectedLocation, "_" ,selectedVariant, "_", "CatchPerGuildOutput","_","Susp_deposit_feeding_benthos.png"),Susp_deposit_feeding_benthos,  width = 12, height = 4, dpi = 600, units = "in", device= "png")
 
-png(filename = paste0(selectedLocation, "_" ,selectedVariant, "_", "CatchPerGuildOutput","_","Carn_scavebge_feeding_benthos.png"))
 Carn_scavebge_feeding_benthos <-     createCatchPlotPerGuild(
       model,
       results = results_baseline,
       dsa = 6
     )
-dev.off()
+ggsave(filename = paste0(selectedLocation, "_" ,selectedVariant, "_", "CatchPerGuildOutput","_","Carn_scavebge_feeding_benthos.png"),Carn_scavebge_feeding_benthos,  width = 12, height = 4, dpi = 600, units = "in", device= "png")
 
-png(filename = paste0(selectedLocation, "_" ,selectedVariant, "_", "CatchPerGuildOutput","_","Pelagic_invertebrates.png"))
 Pelagic_invertebrates <-     createCatchPlotPerGuild(
       model,
       results = results_baseline,
       dsa = 7
     )
-dev.off()
+ggsave(filename = paste0(selectedLocation, "_" ,selectedVariant, "_", "CatchPerGuildOutput","_","Pelagic_invertebrates.png"),Pelagic_invertebrates,  width = 12, height = 4, dpi = 600, units = "in", device= "png")
 
-png(filename = paste0(selectedLocation, "_" ,selectedVariant, "_", "CatchPerGuildOutput","_","birds.png"))
 birds <-     createCatchPlotPerGuild(
       model,
       results = results_baseline,
       dsa = 8
     )
-dev.off()
+ggsave(filename = paste0(selectedLocation, "_" ,selectedVariant, "_", "CatchPerGuildOutput","_","birds.png"),birds,  width = 12, height = 4, dpi = 600, units = "in", device= "png")
 
-png(filename = paste0(selectedLocation, "_" ,selectedVariant, "_", "CatchPerGuildOutput","_","Pinnipeds.png"))
 Pinnipeds <-     createCatchPlotPerGuild(
       model,
       results = results_baseline,
       dsa = 9
     )
-dev.off()
+ggsave(filename = paste0(selectedLocation, "_" ,selectedVariant, "_", "CatchPerGuildOutput","_","Pinnipeds.png"),Pinnipeds,  width = 12, height = 4, dpi = 600, units = "in", device= "png")
 
-png(filename = paste0(selectedLocation, "_" ,selectedVariant, "_", "CatchPerGuildOutput","_","Cetaceans.png"))
 Cetaceans <-     createCatchPlotPerGuild(
       model,
       results = results_baseline,
       dsa = 10
     )
-dev.off()
+ggsave(filename = paste0(selectedLocation, "_" ,selectedVariant, "_", "CatchPerGuildOutput","_","Cetaceans.png"),Cetaceans,  width = 12, height = 4, dpi = 600, units = "in", device= "png")
 
-png(filename = paste0(selectedLocation, "_" ,selectedVariant, "_", "CatchPerGuildOutput","_","Macrophytes.png"))
 Macrophytes <-     createCatchPlotPerGuild(
       model,
       results = results_baseline,
       dsa = 11
     )
-dev.off()
+ggsave(filename = paste0(selectedLocation, "_" ,selectedVariant, "_", "CatchPerGuildOutput","_","Macrophytes.png"),Macrophytes,  width = 12, height = 4, dpi = 600, units = "in", device= "png")
 
-png(filename = paste0(selectedLocation, "_" ,selectedVariant, "_", "CatchPerGuildOutput","_","All_guilds_combined.png"))
 All_guilds_combined <-    createCatchPlotPerGuild(
       model,
       results = results_baseline,
       dsa = 12
     )
-dev.off()
+ggsave(filename = paste0(selectedLocation, "_" ,selectedVariant, "_", "CatchPerGuildOutput","_","All_guilds_combined.png"),All_guilds_combined,  width = 12, height = 4, dpi = 600, units = "in", device= "png")
 
-png(filename = paste0(selectedLocation, "_" ,selectedVariant, "_", "CatchPerGearOutput","_","Pelagic_Trawl_Seine.png"))
 Pelagic_Trawl_Seine <-    createCatchPlotPerGear(
       model,
       results = results_baseline,
       dsa = 1
     )
-dev.off()
+ggsave(filename = paste0(selectedLocation, "_" ,selectedVariant, "_", "CatchPerGearOutput","_","Pelagic_Trawl_Seine.png"),Pelagic_Trawl_Seine,  width = 12, height = 4, dpi = 600, units = "in", device= "png")
 
 if(selectedLocation == "North_Sea"){
-png(filename = paste0(selectedLocation, "_" ,selectedVariant, "_", "CatchPerGearOutput","_","Sandeel_sprat_trawl_Otter30_70mm_TR3.png"))
 Sandeel_sprat_trawl_Otter30_70mm_TR3 <-    createCatchPlotPerGear(
       model,
       results = results_baseline,
       dsa = 2
     )
-dev.off()
+ggsave(filename = paste0(selectedLocation, "_" ,selectedVariant, "_", "CatchPerGearOutput","_","Sandeel_sprat_trawl_Otter30_70mm_TR3.png"),Sandeel_sprat_trawl_Otter30_70mm_TR3,  width = 12, height = 4, dpi = 600, units = "in", device= "png")
 }
 
 if(selectedLocation == "Celtic_Sea"){
-png(filename = paste0(selectedLocation, "_" ,selectedVariant, "_", "CatchPerGearOutput","_","Otter30_70mm_TR3_sandeel_sprat.png"))
 Otter30_70mm_TR3_sandeel_sprat <-    createCatchPlotPerGear(
       model,
       results = results_baseline,
       dsa = 2
     )
-dev.off()
+ggsave(filename = paste0(selectedLocation, "_" ,selectedVariant, "_", "CatchPerGearOutput","_","Otter30_70mm_TR3_sandeel_sprat.png"),Otter30_70mm_TR3_sandeel_sprat,  width = 12, height = 4, dpi = 600, units = "in", device= "png")
 }
 
-png(filename = paste0(selectedLocation, "_" ,selectedVariant, "_", "CatchPerGearOutput","_","Longline_mackerel.png"))
 Longline_mackerel <-    createCatchPlotPerGear(
       model,
       results = results_baseline,
       dsa = 3
     )
-dev.off()
+ggsave(filename = paste0(selectedLocation, "_" ,selectedVariant, "_", "CatchPerGearOutput","_","Longline_mackerel.png"),Longline_mackerel,  width = 12, height = 4, dpi = 600, units = "in", device= "png")
 
-png(filename = paste0(selectedLocation, "_" ,selectedVariant, "_", "CatchPerGearOutput","_","Beam_Trawl_BT1BT2.png"))
 Beam_Trawl_BT1BT2 <-    createCatchPlotPerGear(
       model,
       results = results_baseline,
       dsa = 4
     )
-dev.off()
+ggsave(filename = paste0(selectedLocation, "_" ,selectedVariant, "_", "CatchPerGearOutput","_","Beam_Trawl_BT1BT2.png"),Beam_Trawl_BT1BT2,  width = 12, height = 4, dpi = 600, units = "in", device= "png")
 
-png(filename = paste0(selectedLocation, "_" ,selectedVariant, "_", "CatchPerGearOutput","_","Demersal_Seine.png"))
 Demersal_Seine <-    createCatchPlotPerGear(
       model,
       results = results_baseline,
       dsa = 5
     )
-dev.off()
+ggsave(filename = paste0(selectedLocation, "_" ,selectedVariant, "_", "CatchPerGearOutput","_","Demersal_Seine.png"),Demersal_Seine,  width = 12, height = 4, dpi = 600, units = "in", device= "png")
 
-png(filename = paste0(selectedLocation, "_" ,selectedVariant, "_", "CatchPerGearOutput","_","Demersal_Otter_Trawl_TR1.png"))
 Demersal_Otter_Trawl_TR1 <-    createCatchPlotPerGear(
       model,
       results = results_baseline,
       dsa = 6
     )
-dev.off()
+ggsave(filename = paste0(selectedLocation, "_" ,selectedVariant, "_", "CatchPerGearOutput","_","Demersal_Otter_Trawl_TR1.png"),Demersal_Otter_Trawl_TR1,  width = 12, height = 4, dpi = 600, units = "in", device= "png")
 
-png(filename = paste0(selectedLocation, "_" ,selectedVariant, "_", "CatchPerGearOutput","_","Gill_Nets_Longline_demersal.png"))
 Gill_Nets_Longline_demersal <-    createCatchPlotPerGear(
       model,
       results = results_baseline,
       dsa = 7
     )
-dev.off()
+ggsave(filename = paste0(selectedLocation, "_" ,selectedVariant, "_", "CatchPerGearOutput","_","Gill_Nets_Longline_demersal.png"),Gill_Nets_Longline_demersal,  width = 12, height = 4, dpi = 600, units = "in", device= "png")
 
-png(filename = paste0(selectedLocation, "_" ,selectedVariant, "_", "CatchPerGearOutput","_","Beam_Trawl_shrimp.png"))
 Beam_Trawl_shrimp <-    createCatchPlotPerGear(
       model,
       results = results_baseline,
       dsa = 8
     )
-dev.off()
+ggsave(filename = paste0(selectedLocation, "_" ,selectedVariant, "_", "CatchPerGearOutput","_","Beam_Trawl_shrimp.png"),Beam_Trawl_shrimp,  width = 12, height = 4, dpi = 600, units = "in", device= "png")
 
-png(filename = paste0(selectedLocation, "_" ,selectedVariant, "_", "CatchPerGearOutput","_","Nephrops_Trawl_TR2.png"))
 Nephrops_Trawl_TR2 <-    createCatchPlotPerGear(
       model,
       results = results_baseline,
       dsa = 9
     )
-dev.off()
+ggsave(filename = paste0(selectedLocation, "_" ,selectedVariant, "_", "CatchPerGearOutput","_","Nephrops_Trawl_TR2.png"),Nephrops_Trawl_TR2,  width = 12, height = 4, dpi = 600, units = "in", device= "png")
 
-png(filename = paste0(selectedLocation, "_" ,selectedVariant, "_", "CatchPerGearOutput","_","Creels.png"))
-"Creels"  <-    createCatchPlotPerGear(
+Creels  <-    createCatchPlotPerGear(
       model,
       results = results_baseline,
       dsa = 10
     )
-dev.off()
+ggsave(filename = paste0(selectedLocation, "_" ,selectedVariant, "_", "CatchPerGearOutput","_","Creels.png"),Creels,  width = 12, height = 4, dpi = 600, units = "in", device= "png")
 
-png(filename = paste0(selectedLocation, "_" ,selectedVariant, "_", "CatchPerGearOutput","_","Mollusc_Dredge.png"))
 Mollusc_Dredge <-    createCatchPlotPerGear(
       model,
       results = results_baseline,
       dsa = 11
     )
-dev.off()
+ggsave(filename = paste0(selectedLocation, "_" ,selectedVariant, "_", "CatchPerGearOutput","_","Mollusc_Dredge.png"),Mollusc_Dredge,  width = 12, height = 4, dpi = 600, units = "in", device= "png")
 
 if(selectedLocation == "North_Sea"){
-png(filename = paste0(selectedLocation, "_" ,selectedVariant, "_", "CatchPerGearOutput","_","Whaler.png"))
   Whaler <-    createCatchPlotPerGear(
       model,
       results = results_baseline,
       dsa = 12
     )
-dev.off()
+  ggsave(filename = paste0(selectedLocation, "_" ,selectedVariant, "_", "CatchPerGearOutput","_","Whaler.png"),Whaler,  width = 12, height = 4, dpi = 600, units = "in", device= "png")
 }
 
 if(selectedLocation == "Celtic_Sea"){
-png(filename = paste0(selectedLocation, "_" ,selectedVariant, "_", "CatchPerGearOutput","_","KelpHarvester.png"))
   KelpHarvester <-    createCatchPlotPerGear(
   model,
   results = results_baseline,
   dsa = 12
 )
-dev.off()
+ggsave(filename = paste0(selectedLocation, "_" ,selectedVariant, "_", "CatchPerGearOutput","_","KelpHarvester.png"),KelpHarvester,  width = 12, height = 4, dpi = 600, units = "in", device= "png")
 }
-
 
 
